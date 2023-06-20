754c7b97d41f37950f560bc3e57de8ed9cb73d5b
anshul2510 <anshulchauhan2510@gmail.com>
2023-06-21 00:08:51 +0530

    next commit

--- plantdisease/settings.py
+++ plantdisease/settings.py
@@ -126,7 +126,7 @@ LOGIN_EXEMPT_URLS=['register']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'base/static'),
